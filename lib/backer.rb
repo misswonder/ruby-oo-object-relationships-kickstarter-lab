7fb4a8955443f3603e889058a3847689bc3ba5c6
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-20 22:02:10 -0600

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,18 +2,43 @@ class Backer
 
     attr_reader :name
 
+    # def initialize(name)
+    #     @name = name
+    #     @backed_projects = []
+    # end
+
+    # def back_project(project)
+    #     ProjectBacker.new(project, self)
+    #     @backed_projects << project 
+    # end 
+
+    # def backed_projects
+    #     @backed_projects
+    # end 
+
+    # def initialize(name)
+    #     @name = name
+    #     @project_backers = []
+    # end
+
+    # def back_project(project)
+    #     @project_backers << ProjectBacker.new(project, self) 
+    # end 
+
+    # def backed_projects
+    #     @project_backers.map { |project_backer| project_backer.project }
+    # end 
+
     def initialize(name)
         @name = name
-        @backed_projects = []
     end
 
     def back_project(project)
-        ProjectBacker.new(project, self)
-        @backed_projects << project 
+        ProjectBacker.new(project, self) 
     end 
 
     def backed_projects
-        @backed_projects
+        project_backers = ProjectBacker.all.select { |project_backer| project_backer.backer == self }
+        project_backers.map { |project_backer| project_backer.project }
     end 
-
 end
--- lib/project.rb
+++ lib/project.rb
@@ -2,18 +2,43 @@ class Project
 
     attr_reader :title 
 
+    # def initialize(title)
+    #     @title = title
+    #     @backers = []
+    # end 
+
+    # def add_backer(backer)
+    #     ProjectBacker.new(self, backer)
+    #     @backers << backer 
+    # end 
+
+    # def backers 
+    #     @backers
+    # end
+
+    # def initialize(title)
+    #     @title = title
+    #     @project_backers = []
+    # end 
+
+    # def add_backer(backer)
+    #     @project_backers << ProjectBacker.new(self, backer) 
+    # end 
+
+    # def backers 
+    #     @project_backers.map { |project_backer| project_backer.backer }
+    # end
+
     def initialize(title)
         @title = title
-        @backers = []
-    end 
+    end
 
     def add_backer(backer)
         ProjectBacker.new(self, backer)
-        @backers << backer 
-    end 
-
-    def backers 
-        @backers
     end
 
+    def backers
+        project_backers = ProjectBacker.all.select { |project_backer| project_backer.project == self }
+        project_backers.map { |project_backer| project_backer.backer }
+    end
 end 
